7553112e83bef13d4a235c3c9881af8b679d3fd5
Damon Getsman <damo.gets@gmail.com>
2018-09-09 06:46:58 -0500

    First commit-- initial UI mods done.

---
+++ app/src/main/java/com/example/sprite/dton/ControlPanel.java
@@ -0,0 +1,13 @@
+package com.example.sprite.dton;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ControlPanel extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_control_panel);
+    }
+}
